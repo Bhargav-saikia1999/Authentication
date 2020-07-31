3d57f0148f17e4bf8b5f1fc0fb84d246d13f9741
bhargav-saikia <bhargav.saikia@iitg.ac.in>
2020-07-31 17:06:05 +0530

    Added env variables

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
@@ -18,8 +18,7 @@ const userSchema = new mongoose.Schema({
   email: String
 })
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User",userSchema)
 
